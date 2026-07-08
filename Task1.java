3ff32c4d3f50e0697a3afeec68722d54fe5b3755
Ritika Tyagi <142375391+Ritika-tyagii@users.noreply.github.com>
2026-07-08 20:00:58 +0530

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,12 @@
+public class Task1{
+    public static void main(String[] args) {
+        String name = "Ritika Tyagi";
+        int age = 23;
+        String city = "Modinagar";
+        String college = "RD Engineering College";
+        String course = "Java";
+        String hobby = "Travelling";
+System.out.print("My name is " + name + ".\nI enrolled for " + course + 
+                         ".\n I am " + age + " years old.\n" + "I live in " +city + ".\n" + "My college name   " + college + ".\nI love " + hobby + ".");
+    }
+}
---
+++ Task2.java
@@ -0,0 +1,10 @@
+public class Task2 {
+    public static void main(String[]args){
+        int a=10;
+        if(a>=5){
+            System.out.println("a is greater");
+        }else{
+            System.out.println("a is smaller");
+        }
+    }
+}
