c48c45f8cbf21754e3e15f4ba41c6e4a6d90532f
Ritika Tyagi <142375391+Ritika-tyagii@users.noreply.github.com>
2026-07-08 20:18:02 +0530

    Update GradeCalculator.java

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -1,5 +1,8 @@
 //Student Grade Calculator Write a Java program to take marks of a student as input and display the grade according to the following conditions:
-
+//90 and above(Grade A)
+//75to 89(Grade B)
+//60 to74(Grade C)
+//Bellow 60(GradeD)
 import java.util.Scanner;
 
 public class GradeCalculator {
@@ -9,15 +12,14 @@ Scanner sc = new Scanner(System.in);
 System.out.print("Enter marks: ");
 int marks = sc.nextInt();
 
-if (marks >= 100) {
+if (marks >= 90) {
 System.out.println("Grade A");
 } else if (marks >= 75) {
 System.out.println("Grade B");
-} else if (marks >= 50) {
+} else if (marks >= 60) {
 System.out.println("Grade C");
 } else {
 System.out.println("Grade D");
 }
-
 }
 }
