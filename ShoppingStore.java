73fc4c74e845ae98e17fc69ed84847dc88a5f188
Ritika Tyagi <142375391+Ritika-tyagii@users.noreply.github.com>
2026-08-05 14:11:47 +0530

    commit

--- ShoppingStore.java
+++ ShoppingStore.java
@@ -1,49 +1,46 @@
-package OopsConcept;
 
-//public class OOPSTask {
-    
-//}
+// interface Camera { 
+//     void takePhoto(); 
+//     void recordVideo(); 
+// } 
 
-interface Camera {
-    void takePhoto();
-    void recordVideo();
-}
+// interface MusicPlayer { 
+//     void playMusic(); 
+//     void stopMusic(); 
+// } 
 
-interface MusicPlayer {
-    void playMusic();
-    void stopMusic();
-}
-class Smartphone implements Camera, MusicPlayer {
+// class Smartphone implements Camera, MusicPlayer { 
+//     @Override 
+//     public void takePhoto() { 
+//         System.out.println("Photo captured ."); 
+//     } 
 
-    @Override
-    public void takePhoto() {
-        System.out.println("Photo captured successfully.");
-    }
+//     @Override 
+//     public void recordVideo() { 
+//         System.out.println("Video recording started."); 
+//     } 
 
-    @Override
-    public void recordVideo() {
-        System.out.println("Video recording started.");
-    }
+//     @Override 
+//     public void playMusic() { 
+//         System.out.println("Playing music"); 
+//     } 
 
-    @Override
-    public void playMusic() {
-        System.out.println("Playing music...");
-    }
+//     @Override 
+//     public void stopMusic() { 
+//         System.out.println("Music stopped."); 
+//     } 
+// } 
 
-    @Override
-    public void stopMusic() {
-        System.out.println("Music stopped.");
-    }
+// public class ShoppingStore { 
+//     public static void main(String[] args) { 
+//         Smartphone phone = new Smartphone(); 
+//         phone.takePhoto(); 
+//         phone.recordVideo(); 
+//         phone.playMusic(); 
+//         phone.stopMusic(); 
+//     } 
+// }
 
-    public static void main(String[] args) {
-        Smartphone phone = new Smartphone();
-
-        phone.takePhoto();
-        phone.recordVideo();
-        phone.playMusic();
-        phone.stopMusic();
-    }
-}
 
 
 
@@ -53,68 +50,68 @@ class Smartphone implements Camera, MusicPlayer {
 
 
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// class Product {
-//     private int productId;
-//     private String productName;
-//     private double price;
-//     private int quantity;
+class Product {
+    private int productId;
+    private String productName;
+    private double price;
+    private int quantity;
 
  
-//     Product(int productId, String productName, double price, int quantity) {
-//         this.productId = productId;
-//         this.productName = productName;
+    Product(int productId, String productName, double price, int quantity) {
+        this.productId = productId;
+        this.productName = productName;
 
      
-//         if (price >= 0)
-//             this.price = price;
-//         else
-//             this.price = 0;
+        if (price >= 0)
+            this.price = price;
+        else
+            this.price = 0;
 
-//         if (quantity >= 0)
-//             this.quantity = quantity;
-//         else
-//             this.quantity = 0;
-//     }
+        if (quantity >= 0)
+            this.quantity = quantity;
+        else
+            this.quantity = 0;
+    }
 
-//     public double calculateTotal() {
-//         return price * quantity;
-//     }
+    public double calculateTotal() {
+        return price * quantity;
+    }
 
   
-//     public void display() {
-//         System.out.println("Product Details");
+    public void display() {
+        System.out.println("Product Details");
 
-//         System.out.println("Product ID   : " + productId);
-//         System.out.println("Product Name : " + productName);
-//         System.out.println("Price        : " + price);
-//         System.out.println("Quantity     : " + quantity);
-//         System.out.println("Total Bill   : " + calculateTotal());
-//     }
-// }
+        System.out.println("Product ID   : " + productId);
+        System.out.println("Product Name : " + productName);
+        System.out.println("Price        : " + price);
+        System.out.println("Quantity     : " + quantity);
+        System.out.println("Total Bill   : " + calculateTotal());
+    }
+}
 
-// public class ShoppingStore {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
+public class ShoppingStore {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-//         System.out.print("Enter Product ID: ");
-//         int id = sc.nextInt();
-//         sc.nextLine();
+        System.out.print("Enter Product ID: ");
+        int id = sc.nextInt();
+        sc.nextLine();
 
-//         System.out.print("Enter Product Name: ");
-//         String name = sc.nextLine();
+        System.out.print("Enter Product Name: ");
+        String name = sc.nextLine();
 
-//         System.out.print("Enter Price: ");
-//         double price = sc.nextDouble();
+        System.out.print("Enter Price: ");
+        double price = sc.nextDouble();
 
-//         System.out.print("Enter Quantity: ");
-//         int quantity = sc.nextInt();
+        System.out.print("Enter Quantity: ");
+        int quantity = sc.nextInt();
 
-//         Product p = new Product(id, name, price, quantity);
+        Product p = new Product(id, name, price, quantity);
 
-//         p.display();
+        p.display();
 
-//         sc.close();
-//     }
-// }
+        sc.close();
+    }
+}
